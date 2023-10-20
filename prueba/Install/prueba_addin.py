e9050424faeec2765b0bc279a752d1e2f8c6e0fd
destroyer2003 <danielnunez@ms.uas.edu.mx>
2023-10-19 17:20:33 -0700

    Practica de listas

--- prueba/Install/prueba_addin.py
+++ prueba/Install/prueba_addin.py
@@ -9,6 +9,28 @@ class HerramientaClass(object):
     def onMouseDown(self, x, y, button, shift):
         pass
     def onMouseDownMap(self, x, y, button, shift):
+        pythonaddins.MessageBox("x: "+ str (x)+ "y: "+ str (y), "Coordenadas")
+        coordenada_x= x
+        coordenada_y= y
+       #obtener la referencia a las capas de nuestro mapa en arcmap
+        mapa_mxd = arcpy.mapping.MapDocument("CURRENT")
+
+        #obtener referencia a la vista del mapa
+        vista_mapa =mapa_mxd.activeView
+
+        #generar una referencia espacial para la vista activa
+        referencia_espacial= arcpy.mapping.ListDataFrames(mapa_mxd)[0].spatialReference
+        #obtener el codigo de la referencia espacial
+        codigo_referencia_espacial=referencia_espacial.factoryCode
+        #generar un punto para extraer las coordenas en grados
+        punto = arcpy.Point(coordenada_x,coordenada_y)
+        #asignarle una referencia espacial al punto
+        punto_referenciado = arcpy.PointGeometry(punto, codigo_referencia_espacial)
+        #extraer las coordenadas en grados del punto referenciado
+        coordenadas = punto_referenciado.projectAs(4326)
+#coordenadas en grados
+        pythonaddins.MessageBox("Lat: " + str(coordenadas.firstpoint.X)+ "Lon: " + str(coordenadas.firstpoint.Y), "Coordenadas en grados")
+
         pass
     def onMouseUp(self, x, y, button, shift):
         pass
@@ -19,6 +41,7 @@ class HerramientaClass(object):
     def onMouseMoveMap(self, x, y, button, shift):
         pass
     def onDblClick(self):
+      #  pythonaddins.MessageBox("Hola", "Herramienta")
         pass
     def onKeyDown(self, keycode, shift):
         pass
