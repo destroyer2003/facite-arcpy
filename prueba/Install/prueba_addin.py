4e366f87be09d099f9378747d9cffba8b9078093
destroyer2003 <danielnunez@ms.uas.edu.mx>
2023-10-26 17:20:48 -0700

    Practica de listas

--- prueba/Install/prueba_addin.py
+++ prueba/Install/prueba_addin.py
@@ -1,5 +1,9 @@
+import threading
+import functools
+
 import arcpy
 import pythonaddins
+import webbrowser
 
 class HerramientaClass(object):
     """Implementation for Herramienta.tool (Tool)"""
@@ -9,29 +13,50 @@ class HerramientaClass(object):
     def onMouseDown(self, x, y, button, shift):
         pass
     def onMouseDownMap(self, x, y, button, shift):
-        pythonaddins.MessageBox("x: "+ str (x)+ "y: "+ str (y), "Coordenadas")
-        coordenada_x= x
-        coordenada_y= y
-       #obtener la referencia a las capas de nuestro mapa en arcmap
-        mapa_mxd = arcpy.mapping.MapDocument("CURRENT")
+        #pythonaddins.MessageBox("x: "+ str (x)+ "y: "+ str (y), "Coordenadas")
+        try:
+            coordenada_x= x
+            coordenada_y= y
+           #obtener la referencia a las capas de nuestro mapa en arcmap
+            mapa_mxd = arcpy.mapping.MapDocument("CURRENT")
 
-        #obtener referencia a la vista del mapa
-        vista_mapa =mapa_mxd.activeView
+            #obtener referencia a la vista del mapa
+            vista_mapa =mapa_mxd.activeView
 
-        #generar una referencia espacial para la vista activa
-        referencia_espacial= arcpy.mapping.ListDataFrames(mapa_mxd)[0].spatialReference
-        #obtener el codigo de la referencia espacial
-        codigo_referencia_espacial=referencia_espacial.factoryCode
-        #generar un punto para extraer las coordenas en grados
-        punto = arcpy.Point(coordenada_x,coordenada_y)
-        #asignarle una referencia espacial al punto
-        punto_referenciado = arcpy.PointGeometry(punto, codigo_referencia_espacial)
-        #extraer las coordenadas en grados del punto referenciado
-        coordenadas = punto_referenciado.projectAs(4326)
+            #generar una referencia espacial para la vista activa
+            referencia_espacial= arcpy.mapping.ListDataFrames(mapa_mxd)[0].spatialReference
+
+            #generar un punto para extraer las coordenas en grados
+            punto = arcpy.Point(coordenada_x,coordenada_y)
+            #asignarle una referencia espacial al punto
+            punto_referenciado = arcpy.PointGeometry(punto, referencia_espacial)
+            referencia_espacial_grados = arcpy.SpatialReference(4326)
+            #extraer las coordenadas en grados del punto referenciado
+            coordenadas = punto_referenciado.projectAs(referencia_espacial_grados)
+            x_coord = float(coordenadas.firstPoint.X)
+            y_coord = float(coordenadas.firstPoint.Y)
+            url = "http://maps.google.com/?cbll={0},{1}&cbp=12,90,0,0,5&layer=c".format(y_coord,x_coord)
+           #ejecutar el navegador en segudo plano para que no cuierre arcmap
+            def ejecutar_proceso(function):
+                @functools.wraps(function)
+                def fn (*args, **kwargs):
+                    tarea = threading.Thread(target=function, args=args, kwargs=kwargs)
+                    tarea.start()
+                    tarea.join()
+                return fn
+            Navegador = ejecutar_proceso(webbrowser.open)
+            Navegador(url,new=2)
+
+
+            print (url)
+            arcpy.AddMessage(url)
+        except Exception as e:
+            pythonaddins.MessageBox(e.message, "Error")
+
 #coordenadas en grados
-        pythonaddins.MessageBox("Lat: " + str(coordenadas.firstpoint.X)+ "Lon: " + str(coordenadas.firstpoint.Y), "Coordenadas en grados")
 
-        pass
+
+
     def onMouseUp(self, x, y, button, shift):
         pass
     def onMouseUpMap(self, x, y, button, shift):
